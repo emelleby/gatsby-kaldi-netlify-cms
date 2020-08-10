7ddddd61c128ac6978ec8b459301f592309615af
Eivind Melleby <emelleby@gmail.com>
2020-08-10 13:15:18 +0200

    remove link and change logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import logo from '../img/logo.svg'
+// import logo from '../img/logo.svg'
+import logo from '../img/Logo_Dr_G640.jpg'
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
@@ -10,16 +11,16 @@ import vimeo from '../img/social/vimeo.svg'
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className="footer has-background-black has-text-white-ter">
+      <footer className="footer has-background-primary has-text-white-ter">
         <div className="content has-text-centered">
           <img
             src={logo}
-            alt="Kaldi"
+            alt="Dr. G"
             style={{ width: '14em', height: '10em' }}
           />
         </div>
-        <div className="content has-text-centered has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
+        <div className="content has-text-centered has-background-primary has-text-white-ter">
+          <div className="container has-background-primary has-text-white-ter">
             <div style={{ maxWidth: '100vw' }} className="columns">
               <div className="column is-4">
                 <section className="menu">
@@ -39,11 +40,11 @@ const Footer = class extends React.Component {
                         Products
                       </Link>
                     </li>
-                    <li>
+                    {/*<li>
                       <Link className="navbar-item" to="/contact/examples">
                         Form Examples
                       </Link>
-                    </li>
+                    </li>*/}
                     <li>
                       <a
                         className="navbar-item"
